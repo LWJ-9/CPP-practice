acfc823d76bd131df60a6c3ae3af3f68b168412d
LWJ-9 <lwj9@outlook.com>
2020-05-19 00:10:31 +0900

    second day i guess

--- main.cpp
+++ main.cpp
@@ -10,7 +10,9 @@ int addNumbers(int x, int y){
     int answer = x + y;
     return answer;
 }
-
+int recur(int x, int y){
+    return y==0? x : recur(y,x%y) ;
+}
 class LisClass{
 public:
     LisClass(string z){
@@ -31,6 +33,60 @@ private:
     string name;
 };
 
+void averageAge(){
+    int xx = 1;
+    int number;
+    int total = 0;
+
+    while(xx <= 5 ){
+        cout << "Enter a number you want add" << endl;
+        cin >> number;
+        total = total + number;
+        xx++;
+    }
+
+    cout << "Your total is " << total << endl;
+}
+void peopleEntered(){
+    int age1;
+    int ageTotal = 0;
+    int numberOfPeopleEntered = 0;
+
+    cout << "Enter first persons age or -1 to quit"  << endl;
+    cin >> age1;
+
+    while (age1 != -1){
+        ageTotal += age1;
+        numberOfPeopleEntered += 1;
+
+            cout << "Enter first persons age or -1 to quit" << endl;
+            cin >> age1;
+    }
+
+    cout << "Number of people entered: " << numberOfPeopleEntered << endl;
+    cout << "Average of age:" << ageTotal << endl;
+
+}
+void theSwitchStmt(){
+
+    int age;
+    cout << "input a age" << endl;
+    cin >> age;
+
+    switch(age){
+    case 16:
+        cout << "hey you can drive now!" << endl;
+        break;
+    case 18:
+        cout << "go buy some lotto tickets~!" << endl;
+    case 21:
+        cout << "buy me some beer" << endl;
+        break;
+    default:
+        cout << "sorry you get nothing" << endl;
+    }
+
+}
 
 int main()
 {
@@ -52,6 +108,47 @@ int main()
 
     Burrito bo;
 
-    return 0;
+    //!!--if statement--
+    int x = 11;
+    if(x > 10 && x < 12){
+        cout <<  "omg i am preggy" << endl;;;
+    }
+
+    int age = 110;
+
+    if(age > 60){
+        cout << "wow you are old" << endl;
+
+            if(age >= 100){
+                cout << "why are you still alive?" << endl;
+            }
+    }else{
+        cout << "you are young ,get a job!" << endl;
+    }
+
+    cout << recur(231,15) << endl ;
+
+
+    //!!--while statement--
+
+    int bacon = 0 ;
+
+    while(bacon < 5){
+        cout << "bacon in " << bacon <<endl;
+        bacon++;
+    }
+    int pp=100;
+    cout << pp++ << pp << pp++  << ++pp <<endl;
+    cout << pp << endl;
+
+    //! cin
+
+
+    //averageAge();
+    //peopleEntered();
+    theSwitchStmt();
+
+
+    //return 0;
 }
 
